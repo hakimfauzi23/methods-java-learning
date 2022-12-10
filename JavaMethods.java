aea9aba0542b442043092416e0c6e9b4dcd58cbd
Hanif <hanif.hakim@spesolution.com>
2022-12-10 19:40:58 +0700

    Basic Methods Java

---
+++ JavaMethods.java
@@ -0,0 +1,16 @@
+/**
+ * JavaMethods
+ * You can call methods in main()
+ */
+public class JavaMethods {
+
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+        myMethod();
+    }
+}
